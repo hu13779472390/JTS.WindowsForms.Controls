7d2539706cb4b444f08e2943908d696deabf42f7
Jase <public@jase.online>
2017-01-04 21:39:26 +1100

    Set ControlStyles in CTOR and Designer Properties

--- JTS.WindowsForms.Controls/ProgressBar.cs
+++ JTS.WindowsForms.Controls/ProgressBar.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -26,15 +30,53 @@ namespace JTS.WindowsForms.Controls
         public override Color BackColor { get; set; }
         #endregion
 
-        public ProgressBar()
+        [Browsable(true), Category("Behavior"), Description("Gets or sets the lower bound of the range that this Progress Bar is working with.")]
+        public int Minimum { get; set; }
+
+        [Browsable(true), Category("Behavior"), Description("Gets or sets the upper bound of the range that this Progress Bar is working with.")]
+        public int Maximum { get; set; }
+
+        [Browsable(true), Category("Behavior"), Description("Gets or sets the amount to increment the current value of the ProgressBar when the Indicate() method is called.")]
+        public int Step { get; set; }
+
+        [Browsable(true), Category("Behavior"), Description("Gets or sets the current value of the ProgressBar, in the raneg specified by the Minimum and Maximum properties.")]
+        public int Value { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of this control with its default settings.
+        /// </summary>
+        public ProgressBar() : base()
         {
             InitializeComponent();
 
             this.DoubleBuffered = true;
 
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.UseTextForAccessibility, true);
+            SetStyle(ControlStyles.Selectable, true);
             SetStyle(ControlStyles.CacheText, true);
             SetStyle(ControlStyles.ResizeRedraw, true);
             SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        protected override CreateParams CreateParams
+        {
+            [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+            get
+            {
+                CreateParams createParams = base.CreateParams;
+                createParams.ExStyle |= 0x02000000;
+
+                return createParams;
+            }
+        }
+
+        protected override ImeMode DefaultImeMode
+        {
+            get
+            {
+                return ImeMode.Disable;
+            }
         }
 
         /// <summary>
@@ -50,11 +92,7 @@ namespace JTS.WindowsForms.Controls
 
         protected override void OnPaint(PaintEventArgs pe)
         {
-            // TODO: Draw the Border.
 
-            // TODO: Draw the Background.
-
-            // TODO: Draw a Step.
         }
     }
 }
