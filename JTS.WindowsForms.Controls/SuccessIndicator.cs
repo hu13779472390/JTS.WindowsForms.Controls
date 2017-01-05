367a5e691f7161819c40b512fc57c1949b020ecb
Jase <public@jase.online>
2017-01-05 15:21:33 +1100

    Implemented SuccessIndicator that implements Indicator Interface

---
+++ JTS.WindowsForms.Controls/SuccessIndicator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JTS.WindowsForms.Controls
+{
+    class SuccessIndicator : Indicator
+    {
+        int Indicator.Identifier { get; set; }
+        string Indicator.Operation { get; set; }
+        RectangleF Indicator.Rectangle { get; set; }
+
+        Color Color { get; set; }
+        int Value { get; set; }
+    }
+}
